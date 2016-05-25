8b3f1bfc5865092fab4c4441494addb8e745c529
Justin Grote <justingrote+github@gmail.com>
2016-05-24 21:36:58 -0700

    Change to JSON API

--- Public/Get-TraverseTestStatus.ps1
+++ Public/Get-TraverseTestStatus.ps1
@@ -7,39 +7,65 @@ Retrieves Traverse Test status based on specified criteria.
 This command leverages the Traverse APIs to get the current status of a device.
 #>
 
-    [CmdletBinding(DefaultParameterSetName="testName")]
+    [CmdletBinding(DefaultParameterSetName="deviceName")]
 
     param (
-        #Name of the test you want to retrieve. Regular expressions are supported.
-        [Parameter(ParameterSetName="testName")][Alias("TestName")][String]$Name = '*',
-        #Name of the device that you wish to retrieve the test from. If specified alone, gets all tests associated with this device
-        [Parameter(ParameterSetName="testName")]
         [Parameter(ParameterSetName="deviceName",Mandatory)][String]$DeviceName,
         #Specify the individual serial number of the test you wish to retrieve
-        [Parameter(ParameterSetName="testSerial",Mandatory)][int]$TestSerial,
+        [Parameter(ParameterSetName="testSerial",ValueFromPipelineByPropertyName,Mandatory)][Alias("serialNumber")][int]$TestSerial,
         #Only retrieve tests that match the specified status criteria
-        [ValidateSet('OK','Warning','Critical','Unknown','Unreachable','Suspended')][String]$status,
-        #[SUPERUSER ONLY] Restrict scope of search to what the specified user can see
-        [String]$UserName
+        [ValidateSet('OK','Warning','Critical','Unknown','Unreachable','Suspended')][String]$status
     ) # Param
 
-    $argumentList = @{}
-    switch ($PSCmdlet.ParameterSetName) {
-        "testName" {
-                        $argumentList.testName = $Name
-                        $argumentList.deviceName = $DeviceName
-                   }
-        "deviceName" {
-                        $argumentList.deviceName = $DeviceName
-                   }
-        "testSerial" {$argumentList.testSerial = $testSerial}
+#region JSON
+    process {
+        $argumentList = @{}
+        switch ($PSCmdlet.ParameterSetName) {
+
+            "deviceName" {
+                $argumentList.searchCriterias = @(@{
+                    SearchOption = "DEVICE_NAME"
+                    SearchTerms = $DeviceName
+                })
+            }
+            "testSerial" {
+                $argumentList.searchCriterias = @(@{
+                    SearchOption = "TEST_SERIAL_NUMBER"
+                    SearchTerms = $TestSerial
+                })
+            
+            }
+        }
+
+        (Invoke-TraverseCommand -API JSON "test/getStatuses" $argumentList -Verbose:($PSBoundParameters['Verbose'] -eq $true))
     }
-    if ($Username) {$argumentList.userName = $UserName}
-    if ($testType) {$argumentList.testType = $testType}
-    if ($subType)  {$argumentList.subType = $subType}
 
-    (Invoke-TraverseCommand test.list $argumentList -Verbose:($PSBoundParameters['Verbose'] -eq $true)).data.object
+#endregion JSON
 
+
+#region BVEAPI
+<#
+#THis section is how you would get this info with the BVE API, unfortuantely it does not as of 7.x return results in proper JSON.
+#Preserving for when this gets fixed.
+    process {
+        $argumentList = @{}
+        switch ($PSCmdlet.ParameterSetName) {
+            "testName" {
+                            $argumentList.testName = $Name
+                            $argumentList.deviceName = $DeviceName
+                       }
+            "deviceName" {
+                            $argumentList.deviceName = $DeviceName
+                       }
+            "testSerial" {$argumentList.testSerial = $testSerial}
+        }
+        if ($Username) {$argumentList.userName = $UserName}
+        if ($status) {$argumentList.status = $status}
+        (Invoke-TraverseCommand test.status $argumentList -Verbose:($PSBoundParameters['Verbose'] -eq $true))
+    }
+#>
+
+#endregion BVEAPI
 } #Get-TraverseDevice
 
 
