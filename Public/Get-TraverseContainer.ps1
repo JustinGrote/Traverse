322daf09116ce8b37c5b93fc9cd9d2d9934cd365
Justin Grote <justingrote+github@gmail.com>
2016-07-10 17:32:04 -0700

    Added Get Container/Department/DGE scaffolding

---
+++ Public/Get-TraverseContainer.ps1
@@ -0,0 +1,33 @@
+function Get-TraverseContainer {
+<#
+.SYNOPSIS
+Retrieves Traverse Container Objects
+
+.DESCRIPTION
+
+.NOTES
+
+.EXAMPLE
+
+#>
+
+[CmdletBinding()]
+param (
+) # Param
+
+#region Main
+process {
+    $TraverseCommandParams = @{
+        API="REST"
+        Verbose=($PSBoundParameters['Verbose'] -eq $true)
+        ArgumentList=@{}
+    }
+
+    (Invoke-TraverseCommand -Command 'Container.list' @TraverseCommandParams).data.object 
+}
+
+#endregion Main
+
+} #Get-TraverseContainer
+
+
