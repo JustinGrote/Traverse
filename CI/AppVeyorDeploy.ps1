5dc3fc0c044924cac9febdedf6e73708050c121b
Justin Grote <JustinGrote@users.noreply.github.com>
2016-09-25 21:44:31 -0700

    Add new CI Pipeline (#6)
    * Basic Appveyor Adjustments
    Prior to CookieMonster method
    
    * First Attempt at new build pipeline
    
    * Added new secure NuGetApiKey
    
    * Appveyor CI/CD Revamp Initial Test
    
    * Fix Version
    
    * Remove PSDeploy Section from PSake
    
    * Add Deploy back to PSake
    
    * Fix Default task to use Build
    
    * Add Powershell Module Deploy Step
    
    * Fix Deploy Script
    
    * Fix Deploy True
    
    * Add Deployment Testing [TestDeploy]
    
    * CI Candidate Update
    
    * Fix Release Notes

---
+++ CI/AppVeyorDeploy.ps1
@@ -0,0 +1,11 @@
+#Powershell Module Deployment Script
+
+#Skip deployment if this is not a master branch commit and a version tag is not detected
+if($env:APPVEYOR_REPO_BRANCH -and $env:APPVEYOR_REPO_BRANCH -notmatch 'master')
+{
+    write-host -ForegroundColor yellow "DEPLOY PHASE: Not master branch, skipping"
+    exit
+} elseif ($env:APPVEYOR_REPO_TAG_NAME -match 'v\d') {
+    write-host -ForegroundColor yellow "DEPLOY PHASE: Publishing to Powershell Gallery"
+    Publish-Module -Name (Get-ProjectName) -NuGetApiKey $env:NuGetAPIKey
+}
