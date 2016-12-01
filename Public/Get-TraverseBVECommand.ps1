2169d84cc8586b09e689b6a1e7ec8360a8fe1c7d
Justin Grote <justingrote+github@gmail.com>
2016-12-01 14:04:30 -0800

    Added Logic to Get Noun/Verb/Params

--- Public/Get-TraverseBVECommand.ps1
+++ Public/Get-TraverseBVECommand.ps1
@@ -12,12 +12,25 @@ Generates a list of Traverse BVE Available Commands
 
     $HelpResult | where {$PSItem -notmatch '^End with .quit*'} | foreach {
         #Parse out the various inital elements of the command
-        [pscustomobject][ordered]@{
-            Verb = ($_ -replace '^[a-zA-Z]*[\.]([a-zA-Z]*) .*','$1')
-            Noun = ($_ -replace '(^[a-zA-Z]*)[\. ].*','$1')
-
+        $cmdParts = @{}
+        #Match up the base elements to variables
+        if ($PSItem -match '^(?<noun>[a-zA-z]*)\.?(?<verb>[a-zA-Z]*)\ (?<params>\".*\")' ) {
+            $cmdParts.noun = $Matches['noun'].tolower()
+            if ($cmdParts.verb) {
+                $cmdParts.verb = $Matches['noun'].tolower()
+            } else {
+                $cmdParts.verb = 'invoke'
+            }
+            $cmdParts.params = $Matches['params'].trim()
         }
 
+        [psCustomObject]$cmdParts
 
+<# TODO: Convert to PSObject
+        [pscustomobject][ordered]@{
+            Verb = ($_ -replace '^[a-zA-Z]*?[\.]([a-zA-Z]*?)[$ ].*','$1')
+            Noun = ($_ -replace '(^[a-zA-Z]*?)[\. ].*','$1')
+        }
+#>
     }
 }
