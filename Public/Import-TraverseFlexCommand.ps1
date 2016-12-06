24a302ee64394f1f8c7e931cd47a3ac972f1febc
Justin Grote <justingrote+github@gmail.com>
2016-12-05 23:40:46 -0800

    Basic Import of List Commands Completed

---
+++ Public/Import-TraverseFlexCommand.ps1
@@ -0,0 +1,42 @@
+function Import-TraverseFlexCommand {
+<#
+.SYNOPSIS
+Connects to the FlexBVE Help API and automatically generates commands to use with Traverse
+.DESCRIPTION
+This command automatically generates a Powershell Module with the Traverse FlexAPI commands.
+These commands match the commands in the FlexAPI, with some additional validation and structure
+Documentation for all commands can be found at the Traverse Developers Guide, but there is no inline help generated
+#>
+    [CmdletBinding()]
+    param (
+        #A prefix to append to the commands, similar to Powershell Module Prefix.
+        [String]$Prefix = "TraverseFlex"
+    )
+
+    $commands = Get-TraverseFlexCommand
+
+    $cmdlist = ""
+    #Start with List Commands, because they are easy
+    $cmds = $commands | where verb -match "List"
+    foreach ($cmdItem in $cmds) {
+        $noun = $cmdItem.noun
+        $verb = $cmdItem.verb
+        $command = @"
+function $($cmdItem.verb)-$($cmdItem.noun) {
+    param ()
+    `$result = Invoke-TraverseCommand -Command $noun.$verb
+    if (`$result.data.object) {
+        `$result.data.object
+    } else {
+        `$result
+    }
+}
+
+"@
+        #invoke-expression $command
+        $cmdList += $command
+    }
+    #TODO: Randomly generate path
+    $cmdList > $env:temp\TraverseFlexCommands.psm1
+    import-module $env:temp\TraverseFlexCommands.psm1 -verbose -force -prefix $prefix
+}
