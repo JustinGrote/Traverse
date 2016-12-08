89ba5ab7133b9d201fc74897ea0d7dea9677ff39
Justin Grote <justingrote+github@gmail.com>
2016-12-08 10:48:26 -0800

    Powershell Formatting

--- Public/Import-TraverseFlexCommand.ps1
+++ Public/Import-TraverseFlexCommand.ps1
@@ -13,16 +13,36 @@ Documentation for all commands can be found at the Traverse Developers Guide, bu
         [String]$Prefix = "TraverseFlex"
     )
 
-    $commands = Get-TraverseFlexCommand
+    $commands = Get-TraverseFlexCommand | where {$_.noun}
+
+    #Exclude Create Test and Update Test for now as they require special formatting
+    #TODO: Add special formatting for Create/Update Test
+    $commands = $commands | where {$PSItem.noun -notlike "Test" -and $PSItem.verb -notmatch 'Create|Update'}
 
     $cmdlist = ""
     #Start with List Commands, because they are easy
-    $cmds = $commands | where verb -match "List"
+    $cmds = $commands
     foreach ($cmdItem in $cmds) {
         $noun = $cmdItem.noun
+        $PShellNoun = $noun
         $verb = $cmdItem.verb
+        #Substitute Invalid Nouns for Powershell-Approved ones
+        $PShellVerb = switch ($verb) {
+            "List" {"Get"}
+            "Create" {"New"}
+            "Delete" {"Remove"}
+            "Status" {"Get"; $PShellNoun = $PShellNoun + "Status"}
+            "Members" {"Get"; $PShellNoun = $PShellNoun + "Members"}
+            "Baseline" {"New"; $PShellNoun = $PShellNoun + "Baseline"}
+            "Represent" {"Enter"}
+            "Suppress" {"Disable"}
+            "" {"Invoke"}
+            default {$verb}
+        }
+
+
         $command = @"
-function $($cmdItem.verb)-$($cmdItem.noun) {
+function $PShellVerb-$PShellNoun {
     <#
     .SYNOPSIS
     This is a generated command for the FlexAPI command $noun.$verb
@@ -41,6 +61,8 @@ function $($cmdItem.verb)-$($cmdItem.noun) {
         $cmdList += $command
     }
     #TODO: Randomly generate path
-    $cmdList > $env:temp\$prefix`.psm1
-    import-module $env:temp\$prefix`.psm1 -prefix $prefix
+    $flexModulePath =  $env:temp + "\" + $prefix + ".psm1"
+    remove-item  $flexModulePath -ErrorAction SilentlyContinue
+    $cmdList > $flexModulePath
+    import-module $flexModulePath -prefix $prefix -global
 }
