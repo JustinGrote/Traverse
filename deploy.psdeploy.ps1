6488a06895fe6a8e1c40bf75c65eba7a2e457c4e
Grote <jgrote@allieddigital.net>
2017-09-12 19:15:17 -0700

    Add PSDeploy

---
+++ deploy.psdeploy.ps1
@@ -0,0 +1,16 @@
+# Upload Appveyor Artifacts if we're in AppVeyor
+if(
+    $env:BHProjectName -and $ENV:BHProjectName.Count -eq 1 -and
+    $env:BHBuildSystem -eq 'AppVeyor'
+   )
+{
+    Deploy DeveloperBuild {
+        By AppVeyorModule {
+            FromSource $ENV:BHProjectName
+            To AppVeyor
+            WithOptions @{
+                Version = $env:APPVEYOR_BUILD_VERSION
+            }
+        }
+    }
+}
